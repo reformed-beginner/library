f54f33c9a1d5de54ac99fe751711d6f378970bed
reformed-beginner <reformed_beginner@outlook.com>
2020-03-23 22:37:49 +0800

    Add month 4 for new mercy morning

--- library/NewMorningMercies/script.py
+++ library/NewMorningMercies/script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os
+import os, sys
 
 def getAllDays():
     pass
@@ -10,9 +10,11 @@ if __name__ == '__main__':
     # years = int(years)
     # # 通过判断闰年，获取一年的总天数
     # days_sum = isLeapYear(years)
-    for i in range(1,32):
-        name = '03' + '%02d' % i + '.md'
-        open(name, 'w')
+    for i in range(1,31):
+        template = './template.md'
+        name = '04' + '%02d' % i + '.md'
+        os.system('cp ' + template + ' ' + name)
+        #open(name, 'w')
     # 获取一年的所有日期
     #all_date_list = getAllDays("2000")
     #print(all_date_list)
---
+++ library/NewMorningMercies/template.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title: 晨恩日新 四月日
+categories: NewMorningMercies
+description: day 1
+keywords: 晨恩日新，保罗区普
+comments: false
+permalink: /library/NewMorningMercies/04
+---
+
+## 四月日
+
+### To <br> Do
+
+
+&emsp;&emsp;ToDo
